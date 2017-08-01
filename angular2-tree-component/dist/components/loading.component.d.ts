8b98136e81a7a990a7520451a9ebbc6b22ae89fb
rafaelm <rafael.moreno@xmpie.com>
2017-08-01 10:12:20 +0300

    remove angular2-tree-component

--- angular2-tree-component/dist/components/loading.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { TemplateRef, ElementRef } from '@angular/core';
-export declare class LoadingComponent {
-    private elementRef;
-    template: TemplateRef<any>;
-    constructor(elementRef: ElementRef);
-}
